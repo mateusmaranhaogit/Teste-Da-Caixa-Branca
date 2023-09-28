045a0dcb8db8ad355c1561443c680f26513e79d8
mateusmaranhaogit <maranhao.mateusmacedo1402@gmail.com>
2023-09-28 15:52:54 -0300

    Código adicionado

--- src/TesteCaixaBranca.java
+++ src/TesteCaixaBranca.java
@@ -1,42 +1,34 @@
+package login;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class TesteCaixaBranca {
-    public Connection conectarBD() {
+public class User{
+    public Connection conectarBD(){
         Connection conn = null;
-        try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        try{
+            Class.forName("com.mysql.Driver.Manager").newInstance();
             String url = "jdbc:mysql://127.0.0.1/teste?user=lopes&password=123";
             conn = DriverManager.getConnection(url);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return conn;
-    }
-
-    public String nome = "";
+        }catch (Exception e) { }
+        return conn;}
+    public String nome ="";
     public boolean result = false;
-
-    public boolean VerificarUsuario(String login, String senha) {
-        String sql = " ";
+    public boolean verificarUsuario(String login, String senha){
+        String sql = "";
         Connection conn = conectarBD();
-
-        // INSTRUÇÃO SQL
+        //INSTRUÇÃO SQL
         sql += "select nome from usuarios ";
-        sql += "where login = '" + senha + "';";
-
-        try {
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-            if (rs.next()) {
-                result = true;
-                nome = rs.getString("nome");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result;
+        sql += "where login = " + "'" + login + "'";
+        sql +=" and senha = " + "'" + senha + "';";
+    try{
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+        if (rs.next()) {
+            result = true;
+            nome = rs.getString("nome");}
+        }catch (Exception e) {  }
+        return result; }
     }
-}
